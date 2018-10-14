7894d8de3b65bac673a2a22aa4bd718e0d10fa50
Thami Inaflas <inaflas.thami@gmail.com>
2018-10-14 23:06:21 +0200

    isFriendsWith(user) method added to user

--- src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,6 +1,28 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserTest {
 
+    private static final User PAUL = new User();
+    private static final User BOB = new User();
 
+
+    @Test
+    public void shouldInformWhenUsersAreNotFriends() {
+        final User user = UserBuilder.aUser()
+                .withFriends(BOB)
+                .build();
+        assertThat(user.isFriendsWith(PAUL)).isFalse();
+    }
+
+    @Test
+    public void shouldInformWhenUsersAreFriends() {
+        final User user = UserBuilder.aUser()
+                .withFriends(BOB, PAUL)
+                .build();
+        assertThat(user.isFriendsWith(PAUL)).isTrue();
+    }
 }
